e1e38fb6e57df7a265147e9738ec6b1d367ce363
Rudy Sarmiento <rudy750@gmail.com>
2022-05-07 22:31:59 -0400

    implemented depth first search

--- graph.js
+++ graph.js
@@ -62,3 +62,19 @@ function bfs(start) {
 }
 
 bfs('PHX');
+
+function dfs(start, visited = new Set()) {
+  visited.add(start);
+  const destinations = adjacencyList.get(start);
+  for (const destination of destinations) {
+    if (destination === 'BKK') {
+      console.log(`DFS found Bangkok`);
+      return;
+    }
+    if (!visited.has(destination)) {
+      dfs(destination, visited);
+    }
+  }
+}
+
+dfs('PHX');
